2910f5b26707007581e00fd40eba59b1a486b9b6
Pirasanthan Jesugeevegan <pirasanthanjesugeevegan@Pirasanthans-MacBook-Pro.local>
2022-10-14 11:23:31 +0100

    added test script

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,20 @@
-const { defineConfig } = require("cypress");
-
+const { defineConfig } = require('cypress');
+const getCompareSnapshotsPlugin = require('cypress-visual-regression/dist/plugin');
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      getCompareSnapshotsPlugin(on, config);
+      on('before:browser:launch', (browser = {}, launchOptions) => {
+        if (browser.name === 'chrome' && browser.isHeadless) {
+          launchOptions.preferences.width = 1920;
+          launchOptions.preferences.height = 1080;
+        }
+
+        return launchOptions;
+      });
     },
   },
+  viewportWidth: 1920,
+  viewportHeight: 1080,
 });
---
+++ cypress/e2e/regression-tests.cy.js
@@ -0,0 +1,23 @@
+// Use the 'describe' to give your test a name
+describe('Home', () => {
+  // The 'it' property indicates a certain condition you're expecting
+  it('passes', () => {
+    // Navigate to the homepage (make sure it's running)
+    cy.visit('http://localhost:3000/');
+
+    // Wait for 2 seconds to load page fully
+    cy.wait(2000);
+
+    // Take a screenshot
+    cy.compareSnapshot('movie');
+
+    // Navigate to people's page
+    cy.visit('http://localhost:3000/people');
+
+    // Wait for 2 seconds to load page fully
+    cy.wait(2000);
+
+    // Take a screenshot
+    cy.compareSnapshot('People');
+  });
+});
